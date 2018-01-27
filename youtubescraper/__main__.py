28ea3556403044a6b23196426c3af472c00cb146
Christopher Wells <cwellsny@nycap.rr.com>
2018-01-27 17:08:17 -0500

    Collect videos for all channels into single array

--- youtubescraper/__main__.py
+++ youtubescraper/__main__.py
@@ -2,16 +2,23 @@ import numpy as np
 import sys
 import yapi
 
+NUM_MAX_VIDEOS = 2
+
 def main():
     input_file = sys.argv[1]
     api_key = readfile(sys.argv[2])[0]
-    contents = readfile(input_file)
+    channel_names = readfile(input_file)
 
     api = yapi.YoutubeAPI(api_key)
+    all_videos = np.concatenate(list(get_all_channel_videos(api, channel_names)))
 
-    for channel_name in contents:
+    print all_videos.shape
+
+def get_all_channel_videos(api, channel_names):
+    for channel_name in channel_names:
+        print channel_name
         videos = np.array(list(get_channel_videos_info(api, channel_name)))
-        print videos.shape
+        yield videos
 
 def get_channel_videos_info(api, channel_name):
     channel_playlist_id = get_channel_playlist_id(api, channel_name)
@@ -63,7 +70,7 @@ def get_channel_playlist_id(api, channel_name):
     return api.get_channel_by_name(channel_name).items[0].contentDetails.relatedPlaylists.uploads
 
 def get_channel_videos(api, channel_playlist_id):
-    return [x.contentDetails.videoId for x in api.get_playlist_items_by_playlist_id(channel_playlist_id, max_results=10).items]
+    return [x.contentDetails.videoId for x in api.get_playlist_items_by_playlist_id(channel_playlist_id, max_results=NUM_MAX_VIDEOS).items]
 
 if __name__ == "__main__":
     main()
