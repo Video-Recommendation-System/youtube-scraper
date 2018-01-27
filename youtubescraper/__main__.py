56c37134f4d72c06f1521fa2ac675c9d93ec2160
Christopher Wells <cwellsny@nycap.rr.com>
2018-01-27 14:19:13 -0500

    Implement file reading

--- youtubescraper/__main__.py
+++ youtubescraper/__main__.py
@@ -1,7 +1,16 @@
 import sys
 
 def main():
-    pass
+    input_file = sys.argv[1]
+    contents = readfile(input_file)
+
+    print contents
+
+def readfile(filepath):
+    with open(filepath) as f:
+        content = [x.strip() for x in f.readlines()]
+        return content
+
 
 if __name__ == "__main__":
     main()
